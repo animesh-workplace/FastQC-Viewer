d870b175488e82e9d27132c00f1f3935a684ce98
Dipu Sharma <33049521+DipuSharma@users.noreply.github.com>
2021-10-26 17:19:34 +0530

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-2nd0dwuuqf%r%cfpkv$_)k9-*y$y!!n=*$uginj4=b!37ii(w&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', '10.10.0.70']
+ALLOWED_HOSTS = ['127.0.0.1', '10.10.0.70', 'localhost']
 
 
 # Application definition
