e4b83cc27272c78d5fd924ff90173341615b8899
Dipu Sharma <33049521+DipuSharma@users.noreply.github.com>
2021-10-26 15:18:39 +0530

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2nd0dwuuqf%r%cfpkv$_)k9-*y$y!!n=*$uginj4=b!37ii(w&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '10.10.0.70']
 
 
