5fbbf1f7711afced1f67774d5908dfc10daa1cb0
Dipu Sharma <33049521+DipuSharma@users.noreply.github.com>
2021-11-09 15:56:58 +0530

    commit

--- api/urls.py
+++ api/urls.py
@@ -7,16 +7,13 @@ from django.contrib.auth import views as auth_views
 from django.conf.urls import handler404, handler500
 
 urlpatterns = [
-                  path('', CustomerRegView.as_view(), name="login"),
-                  path('accounts/login/', CustomerRegView.as_view()),
-                  path('profile/', HomeView.as_view(), name="home"),
-                  path('profile/<str:pt>', HomeView.as_view(), name="projectdetail"),
-                  path('profile/<str:dt>/<str:pt>/<str:data>/<str:data1>', views.show_data, name="show"),
-                  path('refresh/', views.data_store, name="main"),
-                  path('multiqc/<str:pro>/<str:ptt>/<str:st>/<str:smmp>', views.multiqc, name="multi"),
-                  path('logout/', views.logout_request, name='logout'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', CustomerRegView.as_view(), name="login"),
+    path('accounts/login/', CustomerRegView.as_view()),
+    path('profile/', HomeView.as_view(), name="home"),
+    path('profile/<str:pt>', HomeView.as_view(), name="projectdetail"),
+    path('profile/<str:dt>/<str:pt>/<str:data>/<str:data1>', views.show_data, name="show"),
+    path('refresh/', views.data_store, name="main"),
+    path('multiqc/<str:pro>/<str:ptt>/<str:st>/<str:smmp>', views.multiqc, name="multi"),
+    path('logout/', views.logout_request, name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'api.views.handler404'
-
-handler500 = 'api.views.handler500'
--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404, handler500
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls'))
