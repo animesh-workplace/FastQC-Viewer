22fc17cad0b79798e3f76e35b709247f56ab81d9
Dipu Sharma <33049521+DipuSharma@users.noreply.github.com>
2021-11-09 16:04:55 +0530

    commit

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import CustomerRegView, HomeView
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from django.contrib.auth import views as auth_views
+
 from django.conf.urls import handler404, handler500
 
 urlpatterns = [
