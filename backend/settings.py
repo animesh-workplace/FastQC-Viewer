1487691bd84ac545ff3e0530b7887a3fd4664598
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-18 14:35:24 +0530

    all hosts

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-2nd0dwuuqf%r%cfpkv$_)k9-*y$y!!n=*$uginj4=b!37ii(w&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['10.0.2.15', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
